4b43d1440843ad9e90b76636258475cdb7709947
519807609 <519807609@qq.com>
2015-08-05 17:49:16 +0800

    merge

--- suite/organization.go
+++ suite/organization.go
@@ -8,16 +8,6 @@ import (
 )
 
 // 应用套件相关操作的 API 地址
-const (
-	suiteTokenURI    = "https://qyapi.weixin.qq.com/cgi-bin/service/get_suite_token"
-	preAuthCodeURI   = "https://qyapi.weixin.qq.com/cgi-bin/service/get_pre_auth_code"
-	authURI          = "https://qy.weixin.qq.com/cgi-bin/loginpage"
-	permanentCodeURI = "https://qyapi.weixin.qq.com/cgi-bin/service/get_permanent_code"
-	authInfoURI      = "https://qyapi.weixin.qq.com/cgi-bin/service/get_auth_info"
-	getAgentURI      = "https://qyapi.weixin.qq.com/cgi-bin/service/get_agent"
-	setAgentURI      = "https://qyapi.weixin.qq.com/cgi-bin/service/set_agent"
-	corpTokenURI     = "https://qyapi.weixin.qq.com/cgi-bin/service/get_corp_token"
-)
 
 // Suite 结构体包含了应用套件的相关操作
 type Suite struct {
